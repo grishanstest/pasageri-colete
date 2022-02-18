6c4ef394b22f6d4037dfcf1f08764e1c60ac25b2
Mardari Grigore <grigore.mardari@sens.media>
2022-02-18 21:39:28 +0200

    Initial Commit

---
+++ lang.js
@@ -0,0 +1,175 @@
+const langArr = {
+    "unit" :  {
+        "ro": "Transport-Pasageri",
+        "ru": "Пассажирский-Транспорт",
+    },
+    "title-nav" :  {
+        "ro": "Transport-Pasageri",
+        "ru": "Пассажирский-Транспорт",
+    },
+    "nav-1": {
+        "ro": "Acasă",
+        "ru": "Домой",
+    },
+    "nav-2": {
+        "ro": "Servicii",
+        "ru": "Услуги",
+    },
+    "nav-3": {
+        "ro": "Ţări",
+        "ru": "Страны",
+    },
+    "nav-4": {
+        "ro": "Facilități",
+        "ru": "Удобства",
+    },
+    "nav-5": {
+        "ro": "Contacte",
+        "ru": "Контакты",
+    },
+    "countries": {
+        "ro": "Transport",
+        "ru": "Транспорт",
+    },
+    "countries-1": {
+        "ro": "MOLDOVA - GERMANIA - Olanda - Belgia",
+        "ru": "МОЛДОВА - ГЕРМАНИЯ - Нидерланды - Бельгия",
+    },
+    "countries-2": {
+        "ro": "Curse regulate de transport al pasagerilor și coletelor de la adresă până la destinație.",
+        "ru": "Регулярные перевозки пассажиров и посылок от адреса до пункта назначения.",
+    },
+    "services-1": {
+        "ro": "Transport pasageri",
+        "ru": "Пассажирский транспорт",
+    },
+    "services-text-1": {
+        "ro": "Compania oferă clienților săi posibilitatea de a călători în siguranță maximă oriunde în Germania, Belgia sau Olanda la prețuri atractive.",
+        "ru": "Компания предлагает своим клиентам возможность путешествовать с максимальной безопасностью в любую точку Германии, Бельгии или Нидерландов по привлекательным ценам.",
+    },
+    "services-2": {
+        "ro": "Transport automobile",
+        "ru": "Автомобильный транспорт",
+    },
+    "services-text-2": {
+        "ro": "Doriti sa va procurati o masina sau poate sa aduceti acasa masina Dvs si aveti nevoie de suport? Va oferim servicii tral si suport informational pentru a livra automobilul Dvs in siguranta.",
+        "ru": "Вы хотите купить машину ? И привезти ее домой и вам нужна поддержка? Мы предлагаем транспортные услуги и информационную поддержку для безопасной доставки вашего автомобиля.",
+    },
+    "services-3": {
+        "ro": "Transport colete",
+        "ru": "Перевозка посылок",
+    },
+    "services-text-3": {
+        "ro": "Suntem un partener de incredere, si va garantam livrarea cu acuratete a coletelor",
+        "ru": "Мы надежный партнер и гарантируем точную доставку ваших посылок",
+    },
+    "countries-section-1": {
+        "ro": "VĂ OFERIM TRANSPORT INTERNAȚIONAL SPRE ȚĂRILE:",
+        "ru": "ПРЕДЛАГАЕМ МЕЖДУНАРОДНЫЕ ПЕРЕВОЗКИ В СТРАНЫ:",
+    },
+    "countries-md": {
+        "ro": "Moldova",
+        "ru": "Молдова",
+    },
+    "countries-ge": {
+        "ro": "Germania",
+        "ru": "Германия",
+    },
+    "countries-ol": {
+        "ro": "Olanda",
+        "ru": "Нидерланды",
+    },
+    "countries-bg": {
+        "ro": "Belgia",
+        "ru": "Бельгия",
+    },
+    "facilities": {
+        "ro": "Facilități",
+        "ru": "Удобства",
+    },
+    "facilities-1": {
+        "ro": "Facilități",
+        "ru": "Удобства",
+    },
+    "facilities-text": {
+        "ro": "VĂ ASIGURĂM CONFORT SI PLĂCERE",
+        "ru": "МЫ ОБЕСПЕЧИВАЕМ КОМФОРТ И УДОВОЛЬСТВИЕ",
+    },
+    "section-1": {
+        "ro": "TV",
+        "ru": "ТВ",
+    },
+    "section-1-1": {
+        "ro": "Vizionind cele mai recente filme si emisiuni, astfel in cît timpul va trece repede si interesant.",
+        "ru": "Самые свежие фильмы и шоу, так что время пролетит быстро и интересно.",
+    },
+    "section-2": {
+        "ro": "PRÂNZ",
+        "ru": "ОБЕД",
+    },
+    "section-2-2": {
+        "ro": "Facem opriri scurte pentru prinz la cafenele sau restaurantele cele mai preferate.",
+        "ru": "Мы делаем короткие остановки на обед в любимых кафе или ресторанах.",
+    },
+    "section-3": {
+        "ro": "DOI ȘOFERI",
+        "ru": "ДВА ВОДИТЕЛЯ",
+    },
+    "section-3-3": {
+        "ro": "Transportul este condus de 2 soferi, care se rinduiesc, astfel incit calatoria sa fie cit mai rapida si sigura.",
+        "ru": "Транспорт управляется двумя профессиональный водителямии, которые меняются по очереди, чтобы поездка была максимально быстрой и безопасной.",
+    },
+    "section-4": {
+        "ro": "WIFI GRATUIT",
+        "ru": "БЕСПЛАТНЫЙ ВАЙ-ФАЙ",
+    },
+    "section-4-4": {
+        "ro": "Pe parcursul călătoriei, vă puteți bucura de acces gratuit la internet prin WiFi.",
+        "ru": "Во время поездки гости могут воспользоваться бесплатным Wi-Fi.",
+    },
+    "section-5": {
+        "ro": "PÂNĂ LA ADRESA",
+        "ru": "ДО АДРЕСА",
+    },
+    "section-5-5": {
+        "ro": "Aducem pe fiecare pasager la adresa necesara.",
+        "ru": "Доставляем каждого пассажира по нужному адресу.",
+    },
+    "section-6": {
+        "ro": "TRANSPORT CONFORTABIL",
+        "ru": "УДОБНЫЙ ТРАНСПОРТ",
+    },
+    "section-6-6": {
+        "ro": "Toate vehiculile noastre indeplinesc standartele moderne de confort si siguranta.",
+        "ru": "Все наши автомобили соответствуют современным стандартам комфорта и безопасности.",
+    },
+    "contact": {
+        "ro": "Contacte",
+        "ru": "Контакты",
+    },
+    "contact-1": {
+        "ro": "Contacte",
+        "ru": "Контакты",
+    },
+    "contact-text": {
+        "ro": "PUTEȚI BENEFICIA DE O CONSULTAȚIE CU PRIVIRE LA TOATE ÎNTREBĂRILE ORICÂND",
+        "ru": "ВЫ МОЖЕТЕ ПОЛУЧИТЬ КОНСУЛЬТАЦИЮ ПО ЛЮБОМУ ВОПРОСУ В ЛЮБОЕ ВРЕМЯ",
+    },
+    "section-email": {
+        "ro": "E-mail",
+        "ru": "Электронная почта",
+    },
+    "section-point": {
+        "ro": "Adresa noastră",
+        "ru": "Наш адрес",
+    },
+    "section-tel": {
+        "ro": "suna-ne",
+        "ru": "позвони нам",
+    },
+    "footer": {
+        "ro": "Transport-Pasageri",
+        "ru": "Пассажирский-Транспорт",
+    },
+
+}
